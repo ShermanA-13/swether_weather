4eb9ce14b1451e2213551e51c6648de4fe95c105
Sherman Au <shermau13@gmail.com>
2022-06-13 08:36:44 -0600

    test: background_facade

---
+++ spec/facades/background_facade_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+RSpec.describe BackgroundFacade do
+  it '.find_image' do
+    image = BackgroundFacade.find_image('Denver,Co')
+    expect(image[:photos][0]).to have_key(:photographer_url)
+    expect(image[:photos][0]).to have_key(:photographer)
+    expect(image[:photos][0][:src]).to have_key(:small)
+  end
+end
