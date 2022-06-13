a01bf085d000573bb17099b81efc8298e6e17ed2
Sherman Au <shermau13@gmail.com>
2022-06-13 10:24:23 -0600

    add: book_search route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/weather', to: 'weather#index'
       get '/backgrounds', to: 'backgrounds#index'
+      get '/book-search', to: 'book_search#index'
     end
   end
 end
