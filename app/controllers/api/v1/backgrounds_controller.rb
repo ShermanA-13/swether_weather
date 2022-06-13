e4192259baef91ecb01c2b0e87d1af70367563bf
Sherman Au <shermau13@gmail.com>
2022-06-13 09:22:51 -0600

    add:background controller

---
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::BackgroundsController < ApplicationController
+  before_action :check_location
+
+  def index
+    background = BackgroundFacade.find_image(@location)
+    render json: BackgroundSerializer.image(background, @location)
+  end
+
+  private
+
+  def check_location
+    if params[:location].present?
+      @location = params[:location]
+    else
+      render status: 404
+    end
+  end
+end
