cea74a2394c1d4585d473e46fa53c6cf78fa4f0e
Sherman Au <shermau13@gmail.com>
2022-06-13 08:26:27 -0600

    add: location service

---
+++ app/services/location_service.rb
@@ -0,0 +1,17 @@
+class LocationService
+  class << self
+    def conn
+      url = 'http://www.mapquestapi.com/geocoding/v1/address?'
+      Faraday.new(url: url) do |faraday|
+        faraday.params['key'] = ENV['map_api_key']
+      end
+    end
+
+    def get_coordinates(location)
+      response = conn.get do |faraday|
+        faraday.params['location'] = location
+      end
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
