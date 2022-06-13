44e65fe61d765346f3c7128a04fb16a7d82afae3
Sherman Au <shermau13@gmail.com>
2022-06-13 08:36:26 -0600

    test: location_facade

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 RSpec.describe LocationFacade do
-  it '.get_coords', :vcr do
+  it '.get_coords' do
     coords = LocationFacade.get_coords('Denver,Co')
 
     expect(coords).to eq({ lat: 39.738453, lng: -104.984853 })
