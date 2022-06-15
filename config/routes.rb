dc7389cd0029254121c723a78eb594786557ebfc
Sherman Au <shermau13@gmail.com>
2022-06-14 23:34:13 -0600

    feat: trip#create

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       get '/book-search', to: 'book_search#index'
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
+      post '/trip', to: 'trip#create'
     end
   end
 end
