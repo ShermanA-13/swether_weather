79f4bdca8e6699b387d9d77ab8e1f8c448534c64
Sherman Au <shermau13@gmail.com>
2022-06-14 22:18:16 -0600

    feat: .get_trip

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,5 +1,9 @@
 class MapService < BaseService
   class << self
+    def url
+      url = 'http://www.mapquestapi.com/'
+    end
+
     def get_coordinates(location)
       response = conn(url).get('geocoding/v1/address?') do |faraday|
         faraday.params['location'] = location
@@ -8,8 +12,13 @@ class MapService < BaseService
       get_json(response)
     end
 
-    def url
-      url = 'http://www.mapquestapi.com/'
+    def get_trip(from, to)
+      response = conn(url).get('/directions/v2/route?') do |faraday|
+        faraday.params['from'] = from
+        faraday.params['to'] = to
+        faraday.params['key'] = ENV['map_api_key']
+      end
+      get_json(response)
     end
   end
 end
