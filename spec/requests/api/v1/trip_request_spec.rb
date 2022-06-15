247c9f49524ff691e0e503670d80a81ff6bafdc0
Sherman Au <shermau13@gmail.com>
2022-06-14 23:55:31 -0600

    test: create more robust testing

--- spec/requests/api/v1/trip_request_spec.rb
+++ spec/requests/api/v1/trip_request_spec.rb
@@ -11,6 +11,27 @@ RSpec.describe 'Trip request' do
 
   it 'returns json response for a road trip when a valid API key is given', :vcr do
     body = {
+      "origin": 'caribou,me',
+      "destination": 'los angles,ca',
+      "api_key": @user.api_key.to_s
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
+    post '/api/v1/trip', headers: headers, params: JSON.generate(body)
+    road_trip = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(201)
+    expect(road_trip).to have_key(:data)
+    expect(road_trip[:data]).to have_key(:id)
+    expect(road_trip[:data]).to have_key(:type)
+    expect(road_trip[:data]).to have_key(:attributes)
+    expect(road_trip[:data][:type]).to eq('roadtrip')
+    expect(road_trip[:data][:attributes]).to have_key(:start_city)
+    expect(road_trip[:data][:attributes]).to have_key(:end_city)
+    expect(road_trip[:data][:attributes]).to have_key(:travel_time)
+    expect(road_trip[:data][:attributes]).to have_key(:weather_at_eta)
+  end
+
+  it 'returns json response for a road trip when a valid API key is given' do
+    body = {
       "origin": 'chicago,il',
       "destination": 'denver,co',
       "api_key": @user.api_key.to_s
@@ -30,6 +51,7 @@ RSpec.describe 'Trip request' do
     expect(road_trip[:data][:attributes]).to have_key(:travel_time)
     expect(road_trip[:data][:attributes]).to have_key(:weather_at_eta)
   end
+
   it 'returns json response for incorrect API key', :vcr do
     body = {
       "origin": 'chicago,il',
@@ -42,6 +64,45 @@ RSpec.describe 'Trip request' do
     road_trip = JSON.parse(response.body, symbolize_names: true)
     expect(response.status).to eq(401)
     expect(road_trip).to eq({ error: 'Invalid API key' })
+  end
+
+  it 'returns json response for missing API key', :vcr do
+    body = {
+      "origin": 'chicago,il',
+      "destination": 'denver,co'
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
+    post '/api/v1/trip', headers: headers, params: JSON.generate(body)
+
+    road_trip = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(400)
+    expect(road_trip).to eq({ error: 'Missing API key' })
+  end
+
+  it 'returns json response for missing params', :vcr do
+    body = {
+      "destination": 'denver,co',
+      "api_key": @user.api_key.to_s
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
+    post '/api/v1/trip', headers: headers, params: JSON.generate(body)
+
+    road_trip = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(400)
+    expect(road_trip).to eq({ error: 'Missing destination or origin locations' })
+  end
+
+  it 'returns json response for missing params', :vcr do
+    body = {
+      "origin": 'chicago,il',
+      "api_key": @user.api_key.to_s
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
+    post '/api/v1/trip', headers: headers, params: JSON.generate(body)
+
+    road_trip = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(400)
+    expect(road_trip).to eq({ error: 'Missing destination or origin locations' })
   end
 
   it 'returns travel time impossible when route cannot be mapped', :vcr do
